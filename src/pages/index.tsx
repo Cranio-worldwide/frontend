72087d13ce3ee74cb01cb47e75b78f655ece47d8
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-03-06 00:09:06 +0400

    started footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./Header/Header";
+import { Footer } from './Footer/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
