8dc17506b73573a227cb9fb7e148afcae1c13e3b
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-03-11 19:30:20 +0300

    feat: layout for about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { About } from '../components/About/About';
 import { Footer } from '../components/Footer/Footer';
 
 export default function Home() {
   return (
     <>
+      <About />
       <Footer />
     </>
   );
