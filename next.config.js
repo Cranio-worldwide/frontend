60d2f644828cf673c766cc94015e027fb97c7fc8
Niko.Sidorov <Niko.Sidorov@x5.ru>
2023-03-13 13:12:56 +0100

    chore: add base scss

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const API_HOST = process.env.API_HOST || '/';
 /** @type {{rewrites(): Promise<[{destination: string, source: string}]>, reactStrictMode: boolean}} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    prependData: '@import "@/styles/base.scss";',
+  },
   async rewrites() {
     return [
       {
