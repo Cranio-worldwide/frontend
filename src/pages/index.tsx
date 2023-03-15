a7028f8271d617e2e869e6b4a34c9f47252de3a4
Niko.Sidorov <Niko.Sidorov@x5.ru>
2023-03-15 17:30:41 +0100

    chore: add api request example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { apiClient } from '../app/request/request';
 import { Footer } from '../components/Footer/Footer';
 
-export default function Home() {
+export default function Home({ prop }) {
+  console.log(prop);
   return (
     <>
       <Footer />
     </>
   );
 }
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    const response = await apiClient.get('/api/404');
+  } catch (e) {
+    console.log(e);
+  }
+
+  return {
+    props: {
+      prop: 'from server',
+    },
+  };
+};
