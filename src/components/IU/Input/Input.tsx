e5b2d8666190af536fc595095de698160458c1f4
Yanarkaev <hasssanbek@gmail.com>
2023-04-11 16:58:46 +0300

    компонент поля ввода

--- src/components/IU/Input/Input.tsx
+++ src/components/IU/Input/Input.tsx
@@ -4,20 +4,35 @@ import cn from 'classnames';
 interface InputProps
   extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   value: string;
-  status?: 'warning' | 'error' | 'success';
-  className?: string;
   placeholder?: string;
+  status?: 'warning' | 'error' | 'success';
   statusText?: string;
+  variant?: 'cut';
+  className?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export const Input = ({ value, status, statusText, className, ...props }: InputProps) => {
+export const Input: React.FC<InputProps> = ({
+  value,
+  onChange,
+  status,
+  statusText,
+  variant = '',
+  className,
+  ...props
+}) => {
   return (
     <div className={cn(styles.container, className)}>
-      <input value={value} className={cn(styles.input, styles[status])} {...props} />
-      {status && (
+      <input
+        value={value}
+        onChange={onChange}
+        className={cn(styles.input, styles[status], styles[variant])}
+        {...props}
+      />
+      {status && statusText && (
         <div className={styles.status}>
           <span className={cn(styles.icon, styles['icon_' + status])}></span>
-          <span>{statusText}</span>
+          <span className={styles.statusText}>{statusText}</span>
         </div>
       )}
     </div>
--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Form } from 'src/components/Form/Form';
-
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import styles from './StartPage.module.scss';
