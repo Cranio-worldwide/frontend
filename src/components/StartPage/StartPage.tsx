b0ef16e865d12a9cdd52628df383756288c55edf
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2023-04-25 09:43:53 +0300

    fix: update about section, add slider

--- src/components/StartPage/StartPage.tsx
+++ src/components/StartPage/StartPage.tsx
@@ -3,6 +3,7 @@ import { TherapistCards } from '@/components/TherapistCards/TherapistCards';
 import { Footer } from '@/components/Footer/Footer';
 import { Header } from '@/components/Header/Header';
 import { Hero } from '@/components/Hero/Hero';
+import { About } from '@/components/About/About';
 import News from '@/components/News/News';
 import styles from './StartPage.module.scss';
 
@@ -18,6 +19,7 @@ export const StartPage = () => {
       <Hero />
       <Form />
       <TherapistCards />
+      <About />
       <News />
       <Footer />
     </>
