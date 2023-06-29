943cda6292933ca06d892ef3711013d7bdc5d4bf
Niko.Sidorov <Niko.Sidorov@x5.ru>
2023-06-29 14:54:38 +0200

    fix proxy

--- src/app/request/request.ts
+++ src/app/request/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: process.env.API_HOST,
+  baseURL: process.env.NODE_ENV !== 'development' ? '/' : process.env.API_HOST,
   withCredentials: true,
 });
 
